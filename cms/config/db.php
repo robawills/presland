845cfc393efcd0558296f344e2b6f0b2913bbf7b
Rob <rob@kbamarketing.co.uk>
2017-08-09 13:45:59 +0100

    Config

--- cms/config/db.php
+++ cms/config/db.php
@@ -12,16 +12,24 @@ return [
     '*' => [
         'server'        => 'localhost',
         'initSQLs'      => ["SET SESSION sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';"],
-        'database'      => 'dean',
+        'database'      => 'presland',
         'tablePrefix'   => 'craft',
     	'user'          => 'presland',
-    	'password'      => 'plumbingisgreat82!'
+    	'password'      => 'a?JkqA(3eJFu/^j-'
+    ],
+
+    '.kbadev.co.uk' => [
+        'database'      => 'dean',
+        'password' => 'plumbingisgreat82!'
     ],
     
     '.local' => [
     	'user' => 'root',
+        'database'      => 'dean',
         'password' => 'root'
     ]
     
 
 ];
+
+
