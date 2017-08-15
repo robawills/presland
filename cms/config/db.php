bb33ea23e11b29102670eeec49b19809ca2dcebe
robawills <rob@kbamarketing.co.uk>
2017-08-15 14:05:54 +0100

    updated db.php so we are now using SSH tunnel

--- cms/config/db.php
+++ cms/config/db.php
@@ -10,7 +10,7 @@
 return [
     
     '*' => [
-        'server'        => 'localhost',
+        'server'        => '127.0.0.1',
         'initSQLs'      => ["SET SESSION sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';"],
         'database'      => 'presland',
         'tablePrefix'   => 'craft',
@@ -19,17 +19,10 @@ return [
     ],
 
     '.kbadev.co.uk' => [
-        'database'      => 'dean',
-        'password' => 'plumbingisgreat82!'
+        'database'  => 'dean',
+        'password'  => 'plumbingisgreat82!'
     ],
     
-    '.local' => [
-    	'user' => 'root',
-        'database'      => 'dean',
-        'password' => 'root'
-    ]
-    
-
 ];
 
 
